454ba794c1d5d7f80fb270de3162233e4b1957f8
krymov.dulat <krymov.dulat@gmail.com>
2023-04-04 17:44:23 +0600

    Added navigation, bnv, menu icons, five fragments, custom colors

--- app/src/main/java/kz/kd/converterapp/MainActivity.kt
+++ app/src/main/java/kz/kd/converterapp/MainActivity.kt
@@ -2,10 +2,28 @@ package kz.kd.converterapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+    private lateinit var bnvMain: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+        navController = navHostFragment.navController
+        bnvMain = findViewById(R.id.bnv_main)
+        bnvMain.setupWithNavController(navController)
     }
 }
