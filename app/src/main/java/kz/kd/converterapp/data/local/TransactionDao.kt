634cb4c06c2389e7bd2d890f2cc4da4fb31e8e51
krymov.dulat <krymov.dulat@gmail.com>
2023-05-01 15:34:34 +0600

    Resolved issue with kapt dependency

--- app/src/main/java/kz/kd/converterapp/data/local/TransactionDao.kt
+++ app/src/main/java/kz/kd/converterapp/data/local/TransactionDao.kt
@@ -1,9 +1,11 @@
 package kz.kd.converterapp.data.local
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
+@Dao
 interface TransactionDao {
     @Query("SELECT * FROM `Transaction`")
     fun getAllTransactions(): List<Transaction>
