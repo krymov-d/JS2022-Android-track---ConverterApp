1373fa5d94d6780e8b34d899e857c2a0dd697fcd
krymov.dulat <krymov.dulat@gmail.com>
2023-05-01 17:26:41 +0600

    Added item currency layout for converter fragment

--- app/src/main/java/kz/kd/converterapp/presentation/converter/ConverterFragment.kt
+++ app/src/main/java/kz/kd/converterapp/presentation/converter/ConverterFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import kz.kd.converterapp.R
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -12,10 +13,22 @@ class ConverterFragment : Fragment() {
 
     private val converterViewModel: ConverterViewModel by viewModel()
 
+    private lateinit var rvConverter: RecyclerView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_converter, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews(view)
+    }
+
+    private fun initViews(view: View) {
+        rvConverter = view.findViewById(R.id.converter_rv)
     }
 }
